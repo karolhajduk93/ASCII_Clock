b8fdbad5b06edb4e5459391a7ef5dad8f3f3d75b
Karol <karolhajduk93@gmail.com>
2018-09-02 09:12:11 +0200

    Adding Digit class with set numbers

--- src/ASCII_Clock.java
+++ src/ASCII_Clock.java
@@ -1,5 +1,10 @@
 public class ASCII_Clock {
 
     public static void main(String[] args) {
+
+        Digit digit = new Digit();
+        for (Character ch: digit.digits.get(9)){
+            System.out.print(ch);
+        }
     }
 }
